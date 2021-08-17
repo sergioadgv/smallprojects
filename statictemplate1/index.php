984533d4aac7b5a97b19d616715ef6258319bed2
sergioadgv <sergioadgv00@gmail.com>
2021-08-17 16:43:41 -0600

    static template 1

---
+++ statictemplate1/index.php
@@ -0,0 +1,347 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,minimum-scale=1.0,maximum-scale=1.0"/>
+	<meta name="description" content="Digital Transformation, Web & App Development and Cybersecurity.">
+	<meta name="keywords" content="Software, programming, digital transformation, IT Services">
+
+	<title>Blue Crow Systems</title>
+
+  
+		<!-- Bootstrap core CSS -->
+		<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+		<!-- Custom styles for this template -->
+		
+		<link href="css/basic.css" rel="stylesheet" type="text/css"> 
+		<!-- Custom styles for this template -->
+		
+		<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>		
+		
+		<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>	 -->
+
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        
+						
+		<script src="js/clicks.js?v=23412"></script>				
+		
+</head>
+
+
+<body>		
+
+            <!--Navigation bar-->
+        	<div id="nav-placeholder">
+        		
+        	</div>
+
+            
+	<br><br><br>
+		<div class="container">			<div class="row text-center myfont">					<div class="col-md-3"></div>			<div class="col-md-6 text-center">							<h3><strong>Digital Transformation Services</strong></h3><br>						</div>			<div class="col-md-1"></div>										</div>		<div class = "row text-center myfont">					<div class="col-md-1"></div>			<div class="col-md-3">							<div class="h-25 p-3 mb-2 bg-secondary text-white">Operations Digitalization</div>				<div class="h-25 p-3 mb-2 bg-secondary text-white">Data-Driven Processes</div>				<div class="h-25 p-3 mb-2 bg-secondary text-white">Information Sharing</div>						</div>			<div class="col-md-4">							<img src="images/company.png">						</div>			<div class="col-md-3">							<div class="h-25 p-3 mb-2 bg-secondary text-white">Digital Tools</div>				<div class="h-25 p-3 mb-2 bg-secondary text-white">Website Development</div>				<div class="h-25 p-3 mb-2 bg-secondary text-white">App Development</div>									</div>			<div class="col-md-1"></div>				</div>			</div><!-- Page Content --><div class="container">  <!-- Portfolio Item Heading -->  <h1 class="my-4">Fabricación Digitalizada</h1>	<!-- Portfolio Item Row -->	<div class="row">    <div class="col-md-8">      <img class="img-fluid" src="images/rotulo.jpg" alt="">    </div>    <div class="col-md-4">      <h3 class="my-3">Letrero en Acero Inoxidable</h3>      <p>Nuestra fabrica cumplirá con los requerimientos mas complejos y detallados de sus proyectos. Ponga su trabajo en nuestras manos y haga sus sueños constructivos una realidad.</p>      <h3 class="my-3">Detalles Técnicos</h3>      <ul>        <li>Acero Inoxidable Certificado (SAE 304)</li>        <li>Corte por Computadora (CNC) +-1mm</li>        <li>Pulido Rayado</li>        <li>Sistema de Iluminación</li>        <li>Transporte e Instalación</li>      </ul>    </div>  </div>  <!-- /.row --></div><!-- /.container --><br>
+
+	<header class="jumbotron rounded-0 text-center text-white m-0">
+
+    <h1>Helping companies digitize and secure their future.</h1>
+
+	</header>
+
+	</div>
+
+
+    <div class="container py-0">
+          <div class="row">
+          <!-- Left Column -->  
+          <div class="col-md-8">
+            <div class="jumbotron p-4 m-2 bg-cover text-white" style="background-image: linear-gradient(to bottom, rgba(0,0,0,0.1) 0%,rgba(0,0,0,0.8) 100%), url(images/inox.webp)">
+					<div class="container">
+					<h2>Fabricación Especializada: Proyectos y Servicios Personalizados</h2>
+					<blockquote>Tenemos 30 años de experiencia en proyectos metálicos y plásticos, industriales y comerciales. Tiene un emprendimiento o proyecto que requiere un servicio de fabricación especializado? Llámenos y le ayudamos a convertir su diseño en una realidad!</blockquote>
+					<br><br><br><br>
+					<a class="btn btn-primary btn-lg" href="contacto">Contáctenos</a>
+            </div>
+          </div>
+          <div class="jumbotron p-4 m-2 bg-cover text-white" style="background-image: linear-gradient(to bottom, rgba(0,0,0,0.6) 0%,rgba(0,0,0,0.8) 100%), url(images/tienda.webp)">
+            <div class="container">
+					<h1 class="text-white">Visite nuestra tienda de productos estandarizados!</h1>
+					<blockquote>Desde maquinaria y articulos para la construcción hasta decoraciones para su hogar. IMICSA, el poder de la industria a su alcance!</blockquote>
+					<br>
+					<br>
+					<br>
+					<br>
+					<a class="btn btn-primary btn-lg" href="tienda">Ir a Tienda!</a>
+             </div>
+          <!-- /.container   -->
+          </div>
+          </div>
+
+			<!-- /.column   -->
+
+
+          <!-- Right Column -->
+          <div class="col-md-4">
+				  <!-- /. Right Box 1   -->
+				  <div class="jumbotron p-4 m-2 bg-cover text-white" style="background-image: linear-gradient(to bottom, rgba(0,0,0,0.6) 0%,rgba(0,0,0,0.6) 100%), url(images/umf.webp)">
+					<div class="container">
+					<h4>Acueductos Industriales</h4>
+					<blockquote>Acueductos industriales en hierro y acero inoxidable. Desde niplería y uniones mecánicas hasta tuberías para proyectos hidroeléctricos.</blockquote>
+					<a class="btn btn-primary btn-lg" href="acueductos">Mas Información</a>
+					  </div>
+				  </div>
+
+				  <!-- /. Right Box 2   -->
+				  <div class="jumbotron p-4 m-2 bg-cover text-white" style="background-image: linear-gradient(to bottom, rgba(0,0,0,0.4) 0%,rgba(0,0,0,0.6) 100%), url(images/tapas.webp)">
+					<div class="container">
+					<h4>Para el Constructor</h4>
+					<blockquote>Desde materiales estructurales hasta tapas metálicas y repuestos. IMICSA ha sido por 30 años aliado del constructor en Costa Rica. </blockquote>
+				 <!--    <hr class="my-4"> -->
+					<!-- <p>It uses utility classes for typography and spacing to space content out within the larger container.</p> -->
+					<a class="btn btn-primary btn-lg" href="metalm">Mas Información</a> <br> <br>
+					  </div>
+				  </div>
+
+				  <!-- /. Right Box 3   -->
+				  <div class="jumbotron p-4 m-2 bg-cover text-white" style="background-image: linear-gradient(to bottom, rgba(0,0,0,0.1) 0%,rgba(0,0,0,0.4) 100%), url(images/impr3d.webp)">
+					<div class="container">
+					<h4>Plásticos 3D y Reciclaje</h4>
+					<blockquote>Repare su maquinaria, decore su casa, imprima sus prototipos y mucho más!</blockquote>
+				 
+					  </div>
+				  </div>
+          </div>
+ 
+
+
+    </div>
+  </div>
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+<div class="container">
+
+ <!-- CARDS -->
+    <div class="row text-center">
+
+      <div class="col-lg-3 col-md-6 mb-4">
+        <div class="card h-100">
+          <img class="card-img-top" src="images/tee.jpg" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Acueductos Industriales</h4>
+            
+          </div>
+          
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-md-6 mb-4">
+        <div class="card h-100">
+          <img class="card-img-top" src="images/pipe.jpg" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Servicios Metalmecánicos</h4>
+            
+          </div>
+          
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-md-6 mb-4">
+        <div class="card h-100">
+          <img class="card-img-top" src="images/agro3.jpg" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Tanques Industriales</h4>
+            </div>
+          
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-md-6 mb-4">
+        <div class="card h-100">
+          <img class="card-img-top" src="images/rotulo.jpg" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Plasma CNC: Paneles Decorativos, rotulos y más.</h4>
+            
+          </div>
+          
+        </div>
+      </div>
+
+    </div>
+
+
+<br>
+
+
+
+
+ <!-- CARDS -->
+    <div class="row text-center">
+
+      <div class="col-lg-3 col-md-6 mb-4">
+        <div class="card h-100">
+          <img class="card-img-top" src="images/soldadura3.jpg" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Soldadura Calificada</h4>
+            </div>
+          
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-md-6 mb-4">
+        <div class="card h-100">
+          <img class="card-img-top" src="images/cnc2.jpg" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Precisión y CNC</h4>
+            
+          </div>
+        
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-md-6 mb-4">
+        <div class="card h-100">
+          <img class="card-img-top" src="images/impr3d.webp" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Impresión 3D y Plásticos Reciclados</h4>
+            </div>
+          
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-md-6 mb-4">
+        <div class="card h-100">
+          <img class="card-img-top" src="images/3d.png" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Modelado 3D y Testing</h4>
+            
+          </div>
+          
+        </div>
+      </div>
+
+    </div>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  <!-- Jumbotron Header -->
+    <header class="jumbotron my-4 text-center p-3">
+      <h2 class="display-6">Productos Estandarizados para la Construcción</h2>
+      
+    </header>
+
+
+
+
+<div class="container">
+ <!-- CARDS -->
+    <div class="row text-center">
+
+      <div class="col-lg-3 col-md-6 mb-4">
+        <div class="card h-100">
+          <img class="card-img-top" src="images/placa.jpg" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Placas de Anclaje</h4>
+            </div>
+          
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-md-6 mb-4">
+        <div class="card h-100">
+          <img class="card-img-top" src="images/tapametalica.png" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Tapas Metálicas</h4>
+           
+          </div>
+          
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-md-6 mb-4">
+        <div class="card h-100">
+          <img class="card-img-top" src="images/precision2.jpg" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Pernos de Anclaje y Tornillos Especiales</h4>
+          </div>
+          
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-md-6 mb-4">
+        <div class="card h-100">
+          <img class="card-img-top" src="images/silletainox.jpg" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Trabajos en Acero Inoxidable</h4>
+            
+          </div>
+          
+        </div>
+      </div>
+
+    </div>
+
+
+</div>
+
+
+
+
+
+
+
+ 		<!-- Cookie Modal -->					
+ 		<div id="mycookiemodal" class="modal" tabindex="-1" role="dialog">		<div class="modal-dialog modal-dialog-centered" role="document">				
+ 		    <div class="modal-content">				  
+ 		    <div class="modal-header bg-dark text-white">					
+ 		    <h5 class="modal-title">Protegemos su Privacidad</h5>					<button type="button" class="close" data-dismiss="modal" aria-label="Close">					  <span aria-hidden="true">&times;</span>					</button>				  </div>				  <div class="modal-body">					<p>Este sitio utiliza cookies y otras tecnologías para dar mantenimiento básico a la pagina. Para ver nuestra politica de seguridad siga el siguiente link.</p>				  
+ 		    </div>				  
+ 		    <div class="modal-footer">					
+ 		    <button id="cookiebutton" type="button" class="btn btn-primary">Entrar</button>					
+ 		    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+ 		    <a class="btn btn-secondary" href="privacidad.php">Política de Privacidad</a>
+ 		    </div>				</div>			  </div>			</div>
+
+
+  
+	<div id="myfooter" class="footr">
+
+	</div>
+	
+	
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+
+</body>
+
+</html>
---
+++ statictemplate1/js/clicks.js
@@ -0,0 +1,85 @@
+$(document).ready(function() {
+    
+	
+	
+	// Load Navbar & Footer
+	$(function(){
+	  $("#nav-placeholder").load("nav.html");
+	  
+	});
+	
+	// Load  Footer
+	$(function(){
+	  $("#myfooter").load("footr.html");
+	  
+	});
+	
+	
+	
+	
+	// setCookie("cookieConsent", "YES", 60);
+	
+	
+	if(getCookie("cookieforConsent") === "YES"){
+		// alert("Cookie is yes");
+	}
+	else if (getCookie("cookieforConsent") === "") {
+		
+		$('#mycookiemodal').modal('show'); 
+		
+	}
+	
+	else{
+		$('#mycookiemodal').modal('show');
+	}
+	
+	
+	$("#cookiebutton").click(function(){
+		
+		// alert("here");
+		setCookie("cookieforConsent", "YES");
+		
+
+		
+		// alert("h");
+		
+		
+		$('#mycookiemodal').modal('hide'); 
+		
+		
+		
+	});
+	
+	
+	// alert(x);
+	
+	
+	
+	function setCookie(cname, cvalue, exdays) {
+		var d = new Date();
+		d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+		var expires = "expires=" + d.toUTCString();
+		document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/; sameSite=Strict";
+	}
+
+	function getCookie(cname) {
+		var name = cname + "=";
+		var decodedCookie = decodeURIComponent(document.cookie);
+		var ca = decodedCookie.split(';');
+		for (var i = 0; i < ca.length; i++) {
+			var c = ca[i];
+			while (c.charAt(0) == ' ') {
+				c = c.substring(1);
+			}
+			if (c.indexOf(name) == 0) {
+				return c.substring(name.length, c.length);
+			}
+		}
+		return "";
+}
+	
+	
+	
+	
+	
+});
